9a261833c90ae95216c71eabc5c9be6c2bdf134a
GrimmOutlook <grimmoutlook87@gmail.com>
2019-03-21 15:13:21 -0400

    Solution works with provided sample data.

--- index.js
+++ index.js
@@ -12,19 +12,38 @@ rl.prompt();
 rl.on('line', cmd => input.push(cmd));
 
 rl.on('close', () => {
-    const email = getEmailAddress(input);
+    const name = getName(input);
     const phone = getPhoneNumber(input);
-    console.log('email: ', email);
-    console.log('phone: ', phone);
+    const email = getEmailAddress(input);
+
+    console.log('Name: ', name);
+    console.log('Phone: ', phone);
+    console.log('Email: ', email);
 
     process.exit(0);
 });
+
+const getName = contactInfo => {
+    const email = getEmailAddress(input);
+    const mailbox = email.substring(0, email.indexOf('@'));
+    let name;
+
+    contactInfo.filter(c => !(c === email) && !(/\d/.test(c))).map(item => {
+        const arr = item.split(' ');
+        const result = arr.map(a => mailbox.includes(a.toLowerCase()));
+        if (result.includes(true)){
+          name = item;
+        }
+        return;
+    });
+    return name;
+};
 
 const getEmailAddress = contactInfo => {
     const regex = new RegExp('@');
     const email = contactInfo.find(item => regex.test(item));
     return email ? email : 'No e-mail found';
-}
+};
 
 const getPhoneNumber = contactInfo => {
     const regexPhonePattern = new RegExp('([0-9]{3})[-. ]?([0-9]{4})');
@@ -36,5 +55,6 @@ const getPhoneNumber = contactInfo => {
 
     const phone = phoneFormatted.join();
 
+    // Strips all non-numeric characters from string:
     return phone.replace(/\D/g,'');
-}
+};
